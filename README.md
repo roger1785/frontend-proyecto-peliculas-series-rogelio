07cf4ae77da92292559463f7dda3bcdc78259687
roger17 <roger17@git.com>
2026-05-21 20:07:08 +0200

    archivos en dev

---
+++ README.md
@@ -0,0 +1,1 @@
+# Frontend Catálogo de Películas y Series
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Catálogo de Películas y Series</h1>
+    </>
+  );
+}
+
+export default App;
